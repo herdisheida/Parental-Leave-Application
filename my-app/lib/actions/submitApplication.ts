e762d566bbb5a847331050ce61efa988e18c0791
Herdis Heida <herdisj24@ru.is>
2026-03-24 19:29:02 +0000

    esrver action

--- my-app/lib/actions/submitApplication.ts
+++ my-app/lib/actions/submitApplication.ts
@@ -1,12 +1,23 @@
 "use server";
 
 export async function submitApplication(data: any) {
-  // simulate a processing delay of 3 seconds
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  try {
+    // simulate a processing delay of 3 seconds
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  // save submission TODO
-  return {
-    success: true,
-    confirmationId: crypto.randomUUID(), // Returns a GUID
-  };
+    //  TODO - delete when done testing
+    // mock random failure (for test)
+    // if (Math.random() > 0.9) throw new Error("Database connection failed");
+
+    // return success with a GUID confirmation number
+    return {
+      success: true,
+      confirmationId: crypto.randomUUID(),
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: "An error occurred while submitting. Please try again.",
+    };
+  }
 }
