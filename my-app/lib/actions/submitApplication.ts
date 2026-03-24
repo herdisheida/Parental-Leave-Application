df9351177ceac2a9bf92e6d9976d96b6c46366c6
Herdis Heida <herdisj24@ru.is>
2026-03-24 20:21:07 +0000

    space

--- my-app/lib/actions/submitApplication.ts
+++ my-app/lib/actions/submitApplication.ts
@@ -2,11 +2,16 @@
 
 import { MasterData } from "../schemas/formSchema";
 
-export async function submitApplication(data: MasterData) {
+export async function submitApplication(formData: FormData) {
   try {
-    // simulate a processing delay of 3 seconds
+    // simulate a processing (3 sec delay)
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
+    const fullName = formData.get("fullName");
+    const files = formData.getAll("files"); // Gets all uploaded files
+
+    console.log(`Processing application for: ${fullName}`);
+    console.log(`Number of files: ${files.length}`);
     //  TODO - delete when done testing
     // mock random failure (for test)
     // if (Math.random() > 0.9) throw new Error("Database connection failed");
