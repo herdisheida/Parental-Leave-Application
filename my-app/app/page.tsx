e56dc51cbfad8fd1388b0fb177d7ac9e18195ac9
Herdis Heida <herdisj24@ru.is>
2026-03-24 18:19:03 +0000

    redirect home page to applicant page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,21 @@
 // redirects to /application/applicant
 
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/application/applicant");
+  }, []);
+
   return (
-    <div>
-      Home page
+    <div className="space-y-8">
+      <h2 className="text-xl font-bold">Home Page</h2>
+      <p className="text-sm text-gray-500">Welcome to the Home Page</p>
     </div>
   );
 }
